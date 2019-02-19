a3e4f23a82934cf1852f0a650fbf1b430009d83c
Qocotzxin <cristianmetchebarne@gmail.com>
2019-02-19 13:57:21 -0300

    Project kickoff

---
+++ src/app/income-outcome/income-outcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-income-outcome',
+  templateUrl: './income-outcome.component.html',
+  styles: []
+})
+export class IncomeOutcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
